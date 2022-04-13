8925d15aa7cdb94dd57140b29f2dd771fa7a0e52
PuemMTH <puem.mth@gmail.com>
2022-04-14 01:27:20 +0700

    main

--- SQL-JWT/middleware/auth.js
+++ SQL-JWT/middleware/auth.js
@@ -9,25 +9,40 @@ verifyToken = async (req, res, next) => {
     
     try {
         const decoded = await jwt.verify(token, config.TOKEN_KEY);
-        // await db.query(`SELECT * FROM jwt_users WHERE email = ?`, [decoded.email], async (err, result) => {
-        //     if (err) throw err;
+        if(decoded.exp < Date.now()){
+            return res.status(401).json({ 
+                msg: 'Token has expired',
+                // DateNow: Date.now(),
+                DateNowFormat: (new Date().getTime() + 1) / 1000,
+                exp: decoded.exp
+            });
+        }
+        
+        const jwt_users = await decoded;
+        req.user = jwt_users;
+        // next();
+        const q =  await db.query(`SELECT * FROM jwt_users WHERE email = ? LIMIT 1`, [decoded.email]);
+        console.log(q);
+        for(let i = 0; i < q.length; i++){
+            if(q[i].email == decoded.email){
+                next();
+            }
+        }
+
+        // if(q.length == 0){
+        //     return res.status(401).json({ msg: 'Invalid token' });
+        // }else{
+        //     console.log("Success");
+        //     // next();
+        // }
+        // .then(result => {
         //     if (result.length == 0) {
         //         return res.status(401).json({ msg: 'Invalid token' });
         //     }
-        //     const jwt_users = await decoded;
+        //     const jwt_users = decoded;
         //     req.user = jwt_users;
-        //     next();
+        //     return next();
         // });
-        db.query(`SELECT * FROM jwt_users WHERE email = ?`, [decoded.email])
-        .then(result => {
-            if (result.length == 0) {
-                return res.status(401).json({ msg: 'Invalid token' });
-            }
-            const jwt_users = decoded;
-            req.user = jwt_users;
-            return next();
-        });
-        
     } catch (err) {
         res.status(401).json({ 
                 msg: 'Token is not valid'
